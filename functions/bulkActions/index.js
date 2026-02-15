6739784a7fa3273d529b5b0f723176264a01eafd
Khomurod <46620174+Khomurod@users.noreply.github.com>
2026-02-16 01:44:32 +0500

    deleted all

--- functions/bulkActions/index.js
+++
@@ -1,9 +0,0 @@
-const sessionController = require('./controllers/sessionController');
-const analyticsService = require('./services/analyticsService');
-const batchWorker = require('./workers/batchWorker');
-
-module.exports = {
-    ...sessionController,
-    ...analyticsService,
-    ...batchWorker
-};
