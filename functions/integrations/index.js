bc95598dc84ffdab66fc39c5c10c4c5fa4d467bd
Khomurod <holmurod96@gmail.com>
2026-02-16 12:09:49 -0600

    deleted all

--- functions/integrations/index.js
+++
@@ -1,8 +0,0 @@
-const configController = require('./controllers/configController');
-const smsService = require('./services/smsService');
-
-// Re-export everything to maintain backward compatibility with functions/index.js
-module.exports = {
-    ...configController,
-    ...smsService
-};
